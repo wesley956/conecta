a5bca81f97a5d230f29b698b939badd66523a02d
wesley956 <aweservicosaw@gmail.com>
2026-07-08 03:02:48 -0300

    feat: ativar segunda camada de redesign do app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/pro-redesign.css";
+import "./styles/pro-redesign-stage2.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
