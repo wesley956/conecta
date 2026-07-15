2038ec10c464a026bf48e251bf41912a86f87128
wesley956 <aweservicosaw@gmail.com>
2026-07-15 19:27:52 -0300

    fix(native): migrate Kotlin configuration for AGP 9.2

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -29,10 +28,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     buildFeatures {
