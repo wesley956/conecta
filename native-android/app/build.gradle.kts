fc4e3ab5f6aab55f93b877cd7b804fce74cdc6c2
wesley956 <aweservicosaw@gmail.com>
2026-07-16 20:31:37 -0300

    chore(android): set release version to 1.1

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ronecaplaytv.nativeapp"
         minSdk = 23
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.1.0"
+        versionCode = 8
+        versionName = "1.1"
 
         buildConfigField(
             "String",
