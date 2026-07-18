d330b1a7a63686952e67fdcdbd5ec0968b122abc
wesley956 <aweservicosaw@gmail.com>
2026-07-18 19:42:54 -0300

    release: set native app version 1.3

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ronecaplaytv.nativeapp"
         minSdk = 23
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.2"
+        versionCode = 10
+        versionName = "1.3"
 
         buildConfigField(
             "String",
