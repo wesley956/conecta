626974366c9c486a0de2780b1b858209c2e1173f
wesley956 <aweservicosaw@gmail.com>
2026-07-20 02:32:55 -0300

    release: preparar ronecaPlayerTV v1.7

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ronecaplaytv.nativeapp"
         minSdk = 23
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.6"
+        versionCode = 14
+        versionName = "1.7"
 
         buildConfigField(
             "String",
