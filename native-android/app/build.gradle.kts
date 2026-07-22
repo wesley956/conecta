5c577fd2edeb9dd27e647d71730eda9e0312cd01
wesley956 <aweservicosaw@gmail.com>
2026-07-22 01:53:45 -0300

    Preparar versão 2.1.1

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ronecaplaytv.nativeapp"
         minSdk = 23
         targetSdk = 36
-        versionCode = 18
-        versionName = "2.1"
+        versionCode = 19
+        versionName = "2.1.1"
 
         buildConfigField(
             "String",
