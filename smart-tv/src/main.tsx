bfdfb58de8767b617feb64e3c9977f20c695c101
wesley956 <aweservicosaw@gmail.com>
2026-07-24 04:01:37 -0300

    Completar experiência Smart TV em main.tsx

--- smart-tv/src/main.tsx
+++ smart-tv/src/main.tsx
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
+import "./experience.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
