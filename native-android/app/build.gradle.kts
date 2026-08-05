344f018ca4e38a1a94469e468f036344b732834c
wesley956 <aweservicosaw@gmail.com>
2026-08-04 22:20:19 -0300

    Prepara Android 2.6.2

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ronecaplaytv.nativeapp"
         minSdk = 23
         targetSdk = 36
-        versionCode = 36
-        versionName = "2.6.1"
+        versionCode = 37
+        versionName = "2.6.2"
 
         buildConfigField(
             "String",
