fcb7a2262f8cac47ec065b214d361d3f71a77d3b
wesley956 <aweservicosaw@gmail.com>
2026-08-09 22:12:28 -0300

    Prepara Android 2.9.1

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ronecaplaytv.nativeapp"
         minSdk = 23
         targetSdk = 36
-        versionCode = 41
-        versionName = "2.9.0"
+        versionCode = 42
+        versionName = "2.9.1"
 
         buildConfigField(
             "String",
