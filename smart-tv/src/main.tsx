590297a5d59acb34e41948e34daeb37f69e01694
wesley956 <aweservicosaw@gmail.com>
2026-08-10 22:44:28 -0300

    LG-06 mount player v2 chrome

--- smart-tv/src/main.tsx
+++ smart-tv/src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { PlayerAspectControl } from "./player/PlayerAspectControl";
 import "./styles.css";
 import "./experience.css";
 import "./parity.css";
 import "./brand.css";
 import "./content.css";
 import "./detail.css";
+import "./player-v2.css";
 import "./navigation.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+    <PlayerAspectControl />
+  </StrictMode>
+);
