40d3bf326979bb094970f38f479249149a8b4b53
wesley956 <aweservicosaw@gmail.com>
2026-08-17 05:11:06 -0300

    WEB-14: include PWA update prompt styling

--- web-player/src/main.tsx
+++ web-player/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { registerPwa } from './pwa';
 import './styles.css';
+import './pwa.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element not found.');
