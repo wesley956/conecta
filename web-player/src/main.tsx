7c34d601124afaed1d815065bdab82a953e5af42
wesley956 <aweservicosaw@gmail.com>
2026-08-17 05:10:38 -0300

    WEB-14: register PWA after app mount

--- web-player/src/main.tsx
+++ web-player/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { registerPwa } from './pwa';
 import './styles.css';
 
 const root = document.getElementById('root');
@@ -11,3 +12,5 @@ createRoot(root).render(
     <App />
   </StrictMode>,
 );
+
+void registerPwa();
