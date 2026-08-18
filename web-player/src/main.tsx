c1982f75a794cc37fcc64e9372754d4eabdb726c
wesley956 <aweservicosaw@gmail.com>
2026-08-17 21:02:55 -0300

    feat(web): wire discovery accessibility layer

--- web-player/src/main.tsx
+++ web-player/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import ExperienceApp from './ExperienceApp';
+import { ExperienceAccessibilityController } from './experienceAccessibility';
 import { registerPwa } from './pwa';
 import './styles.css';
 import './experience.css';
+import './experience-a11y.css';
 import './pwa.css';
 
 const root = document.getElementById('root');
@@ -11,6 +13,7 @@ if (!root) throw new Error('Root element not found.');
 
 createRoot(root).render(
   <StrictMode>
+    <ExperienceAccessibilityController />
     <ExperienceApp />
   </StrictMode>,
 );
