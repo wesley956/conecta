b9cb695ea55e06e98ec11c7a4ffdfb812704db8e
wesley956 <aweservicosaw@gmail.com>
2026-08-18 21:08:56 -0300

    WEB-32/36: restore navigation state and remove duplicate contextual injector

--- web-player/src/main.tsx
+++ web-player/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ContextualHomeEnhancer } from './ContextualHomeEnhancer';
 import ExperienceApp from './ExperienceApp';
 import { ExperienceAccessibilityController } from './experienceAccessibility';
+import { NavigationStateRestorer } from './NavigationStateRestorer';
 import { PwaUpdatePrompt } from './PwaUpdatePrompt';
 import { registerPwa } from './pwa';
 import { SectionNavigationEnhancer } from './SectionNavigationEnhancer';
@@ -17,7 +17,6 @@ import './evolution-batch1.css';
 import './evolution-batch1-integration.css';
 import './evolution-batch2.css';
 import './evolution-batch2-mobile.css';
-import './contextual-home.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element not found.');
@@ -27,11 +26,11 @@ const disposeSplashPolish = installSplashPolish();
 createRoot(root).render(
   <StrictMode>
     <ExperienceAccessibilityController />
+    <NavigationStateRestorer />
     <ExperienceApp />
     <SectionNavigationEnhancer />
     <SettingsPortal />
     <PwaUpdatePrompt />
-    <ContextualHomeEnhancer />
   </StrictMode>,
 );
 
